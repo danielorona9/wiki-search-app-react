ad5756bb54ada66f37e8704fb78d1cfcbc3db1f6
Daniel Orona <danielorona9@users.noreply.github.com>
2019-04-09 13:50:53 -0700

    create App Comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Title from "./components/Title/Title";
+import RandomSearch from "./components/RandomSearch/RandomSearch";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ShowList from "./components/ShowList/ShowList";
+import fetchJsonp from "fetch-jsonp";
+import { Container, Divider, Segment } from "semantic-ui-react";
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    articles: [],
+    selectedArticle: null,
+    fetchSuccess: null
+  };
+
+  onSearchSubmit = searchTerm => {
+    fetchJsonp(
+      `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&continue=-%7C%7C&srsearch=${searchTerm}&srnamespace=0&srlimit=10&sroffset=20&srinfo=&srprop=snippet`,
+      {
+        jsonpCallbackFunction: "searchResults"
+      }
+    )
+      .then(
+        response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Request failed!");
+        },
+        networkError => console.log(networkError.message)
+      )
+      .then(data => {
+        this.setState({ articles: data.query.search, fetchSuccess: true });
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({ fetchSuccess: false });
+      });
+  };
+
+  onArticleSelect = article => {
+    this.setState({ selectedArticle: article });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Title />
+        <Segment basic textAlign={"center"}>
+          <RandomSearch />
+          <Divider horizontal className="textDivColor">
+            Or
+          </Divider>
+          <SearchBar onFormSubmit={this.onSearchSubmit} />
+        </Segment>
+        <ShowList
+          articles={this.state.articles}
+          onArticleSelect={this.onArticleSelect}
+          fetchSuccess={this.state.fetchSuccess}
+        />
+      </Container>
     );
   }
 }
-
-export default App;
