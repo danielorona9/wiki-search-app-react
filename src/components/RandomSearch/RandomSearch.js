7c3d101d9d6d76364968ac25c7afc12b5203b733
Daniel Orona <danielorona9@users.noreply.github.com>
2019-04-09 16:19:45 -0700

    add RandomSearch comp

---
+++ src/components/RandomSearch/RandomSearch.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./RandomSearch.css";
+
+const RandomSearch = () => {
+  return (
+    <a
+      className="ui header medium teal center "
+      href="https://en.wikipedia.org/wiki/Special:Random"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Click Here for Random Article
+    </a>
+  );
+};
+export default RandomSearch;
