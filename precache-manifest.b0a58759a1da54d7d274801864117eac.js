a6abe37b43d5cc3d48ce779db173f1047dc6ec48
Daniel Orona <danielorona9@users.noreply.github.com>
2019-04-08 22:41:22 -0700

    Updates

---
+++ precache-manifest.b0a58759a1da54d7d274801864117eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90d490371390f35d2d3e",
+    "url": "/wiki-search-app-react/static/css/main.95b50513.chunk.css"
+  },
+  {
+    "revision": "90d490371390f35d2d3e",
+    "url": "/wiki-search-app-react/static/js/main.16764b40.chunk.js"
+  },
+  {
+    "revision": "af34cb7b50f3797a5695",
+    "url": "/wiki-search-app-react/static/js/runtime~main.47229b3d.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/wiki-search-app-react/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wiki-search-app-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58681891ca44e2e44f62640448318cc9",
+    "url": "/wiki-search-app-react/index.html"
+  }
+];
