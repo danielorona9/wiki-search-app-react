a79c4f572ea46ca00a09ff50c647b1906ff90c39
Daniel Orona <danielorona9@users.noreply.github.com>
2019-04-09 13:53:51 -0700

    add Title comp

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+import "./Title.css";
+const Title = () => (
+  <div>
+    <Header as="h1" textAlign="center" className="header-title">
+      Wiki Search
+    </Header>
+  </div>
+);
+
+export default Title;
